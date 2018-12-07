15baa0661157a5e366d4f4d0f421b72ce1a90f98
Banjer71 <davgio7@gmail.com>
2018-12-07 12:45:37 +0000

    fix the loop request and reset of the variable

--- script.js
+++ script.js
@@ -1,71 +1,59 @@
-let arr = [];
-function createArr(n){
-  
-    for (let i = 0; i < n; i++) {
-        arr.push(Math.floor(Math.random() * 100) + 1);
-      }
-      return arr;
- }
-
-
 function myFunction(){
-let n = document.getElementById('numb').value;
-let text;
-//let myArr = createArr(n);
- if (n % 1 === 0 && n > 0) {
-  //createArr(n);
-  text = "this is your array " + createArr(n);
-     } else {
-     let n = prompt("It is not a valid number, try again, please");
-     if (n % 1 === 0 && n > 0) {
-        //createArr(n);
-        text = "this is your array " +  createArr(n);
-     } else {
-       text = "end of the game"
-     }
-    }
-    document.getElementById("display-array").innerHTML = text;
-} 
-
-
-
-
-
-
-
-//max
-function checkMax(arr){
-  let max = arr[0];
+  let n = document.getElementById('numb').value;
   let text;
-  for ( let i = 0; i < arr.length; i++) {
-    if(arr[i] > max) {
-    max = arr[i];
-    }
-  } 
-  //return max;
-  document.getElementById("max-min-av").innerHTML = max;
-}
- 
-
-//min
-function checkMin(arr){ 
-let min = arr[0];
-  for ( let i = 0; i < arr.length; i++) {
-    if( arr[i] < min) {
-    min = arr[i];
-    }
-  } 
-    //return min;
-    document.getElementById("max-min-av").innerHTML = min;
-}
+  let arr = [];
+  for (let i = 0; i < 5; i++){
+    if (n % 1 === 0 && n > 0) {
+    text = "this is your array " + createArr(n);
+       } else {
+       n = prompt("It is not a valid number, try again, please");
+       text = "End of the Game, refresh the page, if you want to play again ";
+       } 
+       document.getElementById("display-array").innerHTML = text;
+      }
+  }
 
 
+  function createArr(n){
+     arr = [];
+      for (let i = 0; i < n; i++) {
+          arr.push(Math.floor(Math.random() * 100) + 1);
+        }
+        return arr;
+  }
+  
+
+  //max
+  function checkMax(arr){
+    let max = arr[0];
+    let text;
+    for ( let i = 0; i < arr.length; i++) {
+      if(arr[i] > max) {
+      max = arr[i];
+      }
+    } 
+    document.getElementById("max-min-av").innerHTML = max;
+  }
+   
+  
+  //min
+  function checkMin(arr){ 
+  let min = arr[0];
+    for ( let i = 0; i < arr.length; i++) {
+      if( arr[i] < min) {
+      min = arr[i];
+      }
+    } 
+      document.getElementById("max-min-av").innerHTML = min;
+  }
+  
+  
   //media
   function findMedia(arr){
   let total = 0;
   for (let i = 0; i < arr.length; i++) {
     total += arr[i] / arr.length;
-   }
-   //return total;
-   document.getElementById("max-min-av").innerHTML = total;
+    }
+    document.getElementById("max-min-av").innerHTML = total;
   }
+   
